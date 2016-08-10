488887a23d2164d26ff82082d411db092a983542
skeboking <skeboking@naver.com>
2016-08-10 15:24:08 +0900

    회원가입 기능 구현

--- com/model/UserDBUtil.java
+++ com/model/UserDBUtil.java
@@ -1,5 +1,6 @@
 package com.model;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -7,17 +8,16 @@ import java.util.ArrayList;
  * Created by Junsuk on 2016-08-09.
  */
 public class UserDBUtil {
-    //    DataSource dataSource;
+//    DataSource dataSource;
 //    DriverManager driverManager;
     Connection connection = null;
-
-    public UserDBUtil() {
+    public UserDBUtil(){
         try {
 
             Class.forName("oracle.jdbc.OracleDriver");
             String url = "jdbc:oracle:thin://@192.168.10.230:1521:xe";
-            String username = "hr";
-            String password = "a1234";
+            String username="hr";
+            String password="a1234";
             connection = DriverManager.getConnection(url, username, password);
 
         } catch (SQLException e) {
@@ -28,91 +28,66 @@ public class UserDBUtil {
 
     }
 
-    public ResultSet sqlTransaction(String sql, String[] info) {
+    public ResultSet sqlTransaction(String sql, String id){
 //        Connection connection = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
+
+
         try {
             statement = connection.prepareStatement(sql);
-            for (int i = 1; i <= info.length; i++) {
-                statement.setString(i, info[i]);
-            }
+            statement.setString(1, id);
             resultSet = statement.executeQuery();
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return resultSet;
     }
-
     public Student getStudent(String id, String passwd) {
 
         Student student = null;
         ResultSet resultSet;
         try {
-            String sql = "select * from student where student_id=? AND student_pwd=?";
-            String[] info = new String[]{id, passwd};
-            resultSet = sqlTransaction(sql, info);
+            String sql = "select * from student where student_id=?";
+            resultSet = sqlTransaction(sql,id);
             while (resultSet.next()) {
                 student.setStudentID(resultSet.getString(1));
                 student.setStudentName(resultSet.getString(2));
+
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
-            return null;
         }
         return student;
     }
 
-    public Lecturer getLecturer(String id, String passwd) {
-        Lecturer lecturer = null;
-        ResultSet resultSet;
-
-        String sql = "select * from lecturer where lecturer_id=? AND lecturer_pwd=?";
-        String[] info = new String[]{id, passwd};
-        resultSet = sqlTransaction(sql, info);
-        try {
-            while (resultSet.next()) {
-                lecturer.setLecturerID(resultSet.getString(1));
-                lecturer.setLecturerName(resultSet.getString(2));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return lecturer;
-    }
-
-    public ArrayList<Lecture> getLectures(String id) {
-        ArrayList<Lecture> lectures = new ArrayList<>();
+    public Lecture getLecture(String id) {
         Lecture lecture = null;
         ResultSet resultSet = null;
         String sql = "select * from LECTURE where LECTURE_ID=?";
-        String[] info = new String[]{id};
-        resultSet = sqlTransaction(sql, info);
+        resultSet = sqlTransaction(sql, id);
         try {
             while (resultSet.next()) {
                 String lectureId = resultSet.getString(1);
                 String lectureClass = resultSet.getString(2);
                 String lectureName = resultSet.getString(3);
-                lecture = new Lecture(lectureId, lectureClass, lectureName);
-                lectures.add(lecture);
+                lecture = new Lecture(lectureId,lectureClass,lectureName);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return lectures;
+        return lecture;
     }
 
     public ArrayList<Assignment> getAssignment(String lectureId) {
         ArrayList<Assignment> assignments = new ArrayList<>();
         ResultSet resultSet = null;
         String sql = "select * from assignment where lecture_id=?";
-        String[] info = new String[]{lectureId};
-        resultSet = sqlTransaction(sql, info);
+        resultSet = sqlTransaction(sql, lectureId);
         try {
             while (resultSet.next()) {
-                Assignment assignment = null;
+                Assignment assignment=null;
                 assignment.setAssignmentName(resultSet.getString(1));
                 assignment.setAssignmentDeadlline(resultSet.getString(2));
                 assignments.add(assignment);
@@ -121,14 +96,6 @@ public class UserDBUtil {
             e.printStackTrace();
         }
         return assignments;
-    }
-
-    public void addNewStudent(Student student) {
-
-        String sql = "insert into student value(?, ?, ?, ?)";
-        String[] info = new String[]{student.getId(),student.getName(),student.getLecture(),student.getPasswd()};
-        sqlTransaction(sql,info);
-
     }
 
 
